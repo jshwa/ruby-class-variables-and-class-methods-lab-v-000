8f975853cde233aa19d6b7c604454f06ac30290e
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-06 19:02:19 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @@artist << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] += 1 
+    end
+    genre_count
+  end
+  
+end
